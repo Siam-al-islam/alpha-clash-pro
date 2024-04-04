2ff7472ac0b4f35210c12f74886a5da204ac3ca3
Siam-al-islam <siamalislam692@gmail.com>
2024-04-04 16:59:00 +0600

    play ground score updating done

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -8,11 +8,23 @@ function handleKeyboardButtonPress(event) {
     // check 
     if (pressedKey === expectedAlpabet) {
         console.log('you winnn !!!');
+        // update score 
+        const currentScore = getTextElementValueById('current-score');
+        // new score 
+        const newScore = currentScore + 1;
+        // set score 
+        setElementTextById('current-score', newScore);
+        // start new round 
         removeBackgroundColorById(expectedAlpabet);
         continueGame();
     }
     else {
-        console.log('Try next time')
+        console.log('Try next time');
+        const currentLife = getTextElementValueById('current-life');
+        // new life
+        const newLife = currentLife - 1;
+        // set life 
+        setElementTextById('current-life', newLife);
     }
 }
 // capture keyboard key press 
--- scripts/utility.js
+++ scripts/utility.js
@@ -10,11 +10,25 @@ function showElementById(elementId) {
 
 function setBackgroundColorById(elementId) {
     const element = document.getElementById(elementId);
-    element.classList.add('bg-orange-400')
+    element.classList.add('bg-orange-400');
 }
 function removeBackgroundColorById(elementId) {
     const element = document.getElementById(elementId);
-    element.classList.remove('bg-orange-400')
+    element.classList.remove('bg-orange-400');
+}
+
+// update score 
+function getTextElementValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+
+// set scrore 
+function setElementTextById(elementId, value) {
+    const element = document.getElementById(elementId);
+    element.innerText = value;
 }
 
 function getRandomAlphabet() {
